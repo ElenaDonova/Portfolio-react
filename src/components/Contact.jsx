5c06fade961ed28c848fdc8efd058c305e038e47
Elena Donova <donova.work@yandex.ru>
2023-03-22 21:52:11 +0300

    First commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
       <div className="Contact__container">
         <div className="Contact__left">
           <form ref={ref} onSubmit={handleSubmit} className="Contact__form">
-            <h1 className="Contact__title">Contact Us</h1>
+            <h1 className="Contact__title">Связаться с нами</h1>
             <input name="name" placeholder="Name" className="Contact__input" />
             <input
               name="email"
@@ -46,9 +46,9 @@ const Contact = () => {
               placeholder="Write your message"
               className="Contact__text-area"
             />
-            <button className="Contact__button">Send</button>
+            <button className="Contact__button">Отправить</button>
             {success &&
-              "Your message has been sent. We'll get back to you soon!"}
+              "Ваше сообщение было отправлено. Мы скоро свяжемся с вами!"}
           </form>
         </div>
 
